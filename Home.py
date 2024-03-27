eabf5f595162d700f3ad9d396887f830aeb00567
Brian Roepke <broepke@gmail.com>
2024-03-27 00:09:51 -0500

    Ace of Spades

--- Home.py
+++ Home.py
@@ -1,4 +1,14 @@
+"""
+Streamlit main app
+"""
+
 import streamlit as st
 
 
-st.title("Ace of Spades")
+st.set_page_config(page_title="Deadpool", page_icon=":spades:")
+
+
+# --- Main Application Code
+st.title("Ace of Spades :spades:")
+
+st.image("ace_of_spades.png", "Ace of Spades")
